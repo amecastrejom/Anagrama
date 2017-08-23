1c3aa66ff4f71972a10854bed4abc2b9d81fc0aa
americacastrejon <americacastrejon@10.0.0.6>
2017-08-22 21:57:22 -0500

    Lista con estructuras ligadas

--- Documents/escuela/ITAM /3 semestre/estructuras/Lista/src/lista/Lista.java
+++ Documents/escuela/ITAM /3 semestre/estructuras/Lista/src/lista/Lista.java
@@ -35,6 +35,23 @@ public class Lista<T> {
         return cabeza == null;
     }
     
+    /* INVERSO PILA 
+     public void inversoPila(){
+         Stack<T> pila = new Stack<T>();
+         Nodo<T> current=primero.getSiguiente();
+         for(int i=0; i<cuantosElementos()-1; i++){
+             pila.push(current.getDato());
+             current=current.getSiguiente();
+         }
+         
+         while(!pila.isEmpty()){
+             System.out.print(pila.pop()+ "\t");
+         }
+         
+    }
+     */
+     
+    //INVERSO PILA RECURSIVO 
     public void inversoPila(){
          Stack<T> pila = new Stack<T>();
          Nodo<T> actual=cabeza.getSig();
@@ -56,6 +73,19 @@ public class Lista<T> {
         System.out.println(actual.getDato());
     }
     
+    public void invierteElem(){
+         Stack<T> pila = new Stack<T>();
+         Nodo<T> actual=cabeza.getSig();
+         for(int i=0; i<cont-1; i++){
+             pila.push(actual.getDato());
+             actual=actual.getSig();
+         }
+         
+        invierteElem(actual);
+         
+    }
+    
+    //INVIERTE ELEMENTOS
     private Nodo<T> invierteElem(Nodo<T> actual){
         T elem;
         if(actual==null){
@@ -65,6 +95,25 @@ public class Lista<T> {
         Nodo<T> temp=invierteElem(actual.getSig());
         temp.setDato(elem);
         return temp.getSig();
+    }
+    
+    //INVIERTE LISTA
+    private Nodo<T> invierteLista(Nodo<T> actual){
+        if(actual==null){
+            return actual;
+        }
+        if(actual.getSig()==null){
+            cabeza.setSig(actual);
+            return actual;
+        }
+        Nodo<T> sig=invierteLista(actual.getSig());
+        sig.setSig(actual);
+        actual.setSig(null);
+        return actual;
+    }
+    
+    public void invierte(){
+        invierteLista(cabeza.getSig());
     }
 
     
@@ -81,6 +130,8 @@ public class Lista<T> {
          
         //Mostramos la lista
         lista.inversoPila(); 
+        lista.invierteElem();
+        lista.invierte();
         
          
          
